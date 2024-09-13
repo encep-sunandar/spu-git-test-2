1fda65a40d3db5cb820bccb539172bf0f9d74202
sunanden <encepsunandar@gmail.com>
2024-09-13 21:13:38 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     helo dunia
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet recusandae, id aperiam ipsam repellendus, neque hic voluptates dignissimos mollitia deserunt libero ratione, cupiditate eum! Ratione praesentium veritatis consectetur quidem nulla!</p>
 </body>
 </html>
